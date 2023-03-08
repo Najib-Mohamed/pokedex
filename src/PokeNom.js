d1ebb511b5f7c51d421ca9ba09cf94e9da49064f
Najib-Mohamed <mohanin24@gmail.com>
2023-03-08 09:29:48 +0100

    version finale

--- src/PokeNom.js
+++ src/PokeNom.js
@@ -4,7 +4,7 @@ import image from "./image/pokeball.jpg"
 export default function PokeNom(props) {
   
   return (
-    <Link state={{data : props.data}} to={"/details/:id"} className="flex justify-evenly gap-5 mt-5 cursor-pointer" onMouseOver={(e)=>{e.preventDefault(); props.image(props.data.image)}}>
+    <Link state={{data : props.data}} to={"/details/" + props.data.name} className="flex justify-evenly gap-5 mt-5 cursor-pointer" onMouseOver={(e)=>{e.preventDefault(); props.image(props.data.image)}}>
       <p>NO. {props.data.id} </p>
       <div className="flex justify-between w-60">
       <p>{props.data.name}</p>
